d5c0f228d5588f336d8a062525cd9f72c6b27f9a
Shravan0897 <shravanthakur004@gmail.com>
2023-07-17 15:18:39 +0530

    First test

---
+++ cypress/e2e/shravan/cypressPractice/Demo.cy.js
@@ -0,0 +1,16 @@
+/// <reference types="Cypress" />
+ 
+describe('My First Test Suite', function() 
+{
+ 
+it('My FirstTest case',function() {
+ 
+ 
+cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+cy.get('.search-keyword').type('ca')
+cy.wait(2000)
+//selenium get hit url in browser, cypress get acts like findElement of selenium
+cy.get('.product').should('have.length',5)
+cy.get('.product:visible').should('have.length',4)
+})
+})
