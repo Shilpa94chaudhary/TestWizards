17950a5aa371196fd30eab8c3a1f8102610c1270
Shilpa Chaudhary <shilpa.94chaudhary@gmail.com>
2023-07-25 12:53:27 +0530

    GreenKart test update

---
+++ cypress/e2e/shilpa/cypressPractice/automationPractice.cy.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+describe("Automation Practice website test cases", () => {
+  beforeEach(() => {
+    cy.viewport(1280, 800);
+    cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+  });
+
+  it("", () => {});
+});
