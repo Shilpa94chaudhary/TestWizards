496a9072960af1303f33f801c1fd4fb75a188fe4
Shravan0897 <shravanthakur004@gmail.com>
2023-07-26 17:43:07 +0530

    Script add for muting XHR calls

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -18,3 +18,19 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+if (Cypress.config("hideXHRInCommandLog")) {
+    const app = window.top;
+  
+    if (
+      app &&
+      !app.document.head.querySelector("[data-hide-command-log-request]")
+    ) {
+      const style = app.document.createElement("style");
+      style.innerHTML =
+        ".command-name-request, .command-name-xhr { display: none }";
+      style.setAttribute("data-hide-command-log-request", "");
+  
+      app.document.head.appendChild(style);
+    }
+  }
