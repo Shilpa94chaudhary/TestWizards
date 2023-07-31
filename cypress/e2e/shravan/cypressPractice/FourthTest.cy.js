edb2c3748f5da14906e045c06717cf7b2586e29d
Shravan0897 <shravanthakur004@gmail.com>
2023-07-31 12:33:41 +0530

    Sectio-7 Go-Back funtion

--- cypress/e2e/shravan/cypressPractice/FourthTest.cy.js
+++ cypress/e2e/shravan/cypressPractice/FourthTest.cy.js
@@ -1,11 +1,10 @@
 /// <reference types="Cypress" />
-describe('My Third Test Suite', function() 
+describe('My Fourth Test Suite', function() 
 {
- 
-it('My ThirdTest case',function() {
- 
- 
-cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+//  beforeEach(function(){
+//     cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+//  })
+it('Popup case',function() {
 cy.get('#alertbtn').click()
 cy.get('[value="Confirm"]').click()
 cy.on('window:alert',(str)=>{
@@ -14,5 +13,22 @@ cy.on('window:alert',(str)=>{
 cy.on('window:confirm',(str)=>{
     expect(str).to.equal('Hello , Are you sure you want to confirm?')
 })
+})
+
+it('New Tab Case', function(){
+    cy.get('#opentab').invoke('removeAttr','target').click()
+    //getting error for below code
+    //cy.go('back')
+    // cy.wait(3000)
+    // cy.go('forward')
+})
+// go back function
+it.only('Demo test', function(){
+    cy.visit('https://www.youtube.com/')
+    cy.wait(5000);
+    cy.get("a[title='Library']").click()
+    cy.wait(5000)
+    cy.url().should('include','library')
+    cy.go('back')
 })
 })
